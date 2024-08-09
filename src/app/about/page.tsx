f1642f025eb7a73f430511e700c17382300eb222
muliadev3 <softhouse3@themulia.com>
2024-08-09 15:29:13 +0800

    update: fecthing data destination

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-function about() {
-  return (
-    <div>about</div>
-  )
-}
-
-export default about
--- src/app/components/Destination.tsx
+++ src/app/components/Destination.tsx
@@ -1,8 +1,30 @@
 "use client";
+import React, { useState, useEffect } from 'react';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
-
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 export default function Destination() {
+  const [itineraries, setItineraries] = useState([]);
+console.log(itineraries);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://pandooin.com/api/zamrood/itinerary?highlight=true');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setItineraries(data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
+    fetchData();
+  }, []);
+  
   return (
     <>
     <section className="p-4 w-full max-w-7xl mx-auto">
@@ -26,96 +48,64 @@ export default function Destination() {
           </a>
         </div>
       </div>
+     
       <div className="w-full flex flex-col">
-        <div className="py-4 lg:py-[72px] w-full flex flex-col lg:odd:flex-row-reverse lg:flex-row gap-4 lg:gap-6 items-stretch">
-         
-          
-          <div className="lg:w-1/2 flex flex-col justify-between space-y-2 lg:space-y-4 text-center lg:text-left">
-            <div className="w-full flex flex-col space-y-2">
-                <span className="text-left text-xs lg:text-base">7 DAYS 6 NIGHTS</span>
-                <strong className="text-left text-dark-aquaman text-base lg:text-4xl font-bold line-clamp-2">Paradise Gateway: Labuan Bajo</strong>
-                <span className="text-left text-dark-teal text-sm lg:text-base line-clamp-1 font-bold">Organized by Pandooin</span>
-                <p className="text-left text-sm lg:text-base text-dark-teal line-clamp-4">Labuan Bajo is a tropical paradise nestled in the westernmost part of Flores Island, Indonesia. With its stunning landscapes, crystal-clear waters, and vibrant marine life, it's a gateway to explore the mesmerizing Komodo National Park.</p>
-            </div>
-            <div className="mt-auto w-full inline-flex justify-between items-center">
-                <div className="flex flex-col">
-                    <span className="text-left text-dark-teal text-xs lg:text-base">Start from</span>
-                    <span className="text-gray-400 line-through font-medium text-sm lg:text-3xl">IDR 9,999,999</span>
-                    <span className="text-left font-unbounded text-dark-teal text-lg lg:text-[28px] font-medium">IDR 5,200,000</span>
-                </div>
-                <a href="#" className="mt-10">
-                    <button className="button text-center inline-flex justify-center items-center px-6 py-2.5 rounded-full capitalize font-bold text-sm lg:text-base transition-colors ease-in-out duration-300 bg-transparent text-dark-teal border-2 border-dark-teal hover:bg-dark-teal hover:text-vista-white hover:border-dark-teal ">
-                    See Details
-                    </button>
-                </a>
-            </div>
-          </div>
-          <div className="relative lg:w-1/2 max-h-[256px] lg:max-h-[327px] aspect-video">
-            <img
-              src="/Destination1.png"
-              alt=""
-              className="object-cover object-center transition-opacity ease-in-out duration-300 opacity-100"
-            />
-          </div>
-        </div>
-        <div className="py-4 lg:py-[72px] w-full flex flex-col lg:odd:flex-row-reverse lg:flex-row gap-4 lg:gap-6 items-stretch">
-          <div className="lg:w-1/2 flex flex-col justify-between space-y-2 lg:space-y-4 text-center lg:text-left">
-            <div className="w-full flex flex-col space-y-2">
-                <span className="text-left text-xs lg:text-base">7 DAYS 6 NIGHTS</span>
-                <strong className="text-left text-dark-aquaman text-base lg:text-4xl font-bold line-clamp-2">Paradise Gateway: Labuan Bajo</strong>
-                <span className="text-left text-dark-teal text-sm lg:text-base line-clamp-1 font-bold">Organized by Pandooin</span>
-                <p className="text-left text-sm lg:text-base text-dark-teal line-clamp-4">Labuan Bajo is a tropical paradise nestled in the westernmost part of Flores Island, Indonesia. With its stunning landscapes, crystal-clear waters, and vibrant marine life, it's a gateway to explore the mesmerizing Komodo National Park.</p>
-            </div>
-            <div className="mt-auto w-full inline-flex justify-between items-center">
-                <div className="flex flex-col">
-                    <span className="text-left text-dark-teal text-xs lg:text-base">Start from</span>
-                    <span className="text-gray-400 line-through font-medium text-sm lg:text-3xl">IDR 9,999,999</span>
-                    <span className="text-left font-unbounded text-dark-teal text-lg lg:text-[28px] font-medium">IDR 5,200,000</span>
-                </div>
-                <a href="#" className="mt-10">
-                    <button className="button text-center inline-flex justify-center items-center px-6 py-2.5 rounded-full capitalize font-bold text-sm lg:text-base transition-colors ease-in-out duration-300 bg-transparent text-dark-teal border-2 border-dark-teal hover:bg-dark-teal hover:text-vista-white hover:border-dark-teal ">
-                    See Details
-                    </button>
-                </a>
-            </div>
-          </div>
-          <div className="relative lg:w-1/2 max-h-[256px] lg:max-h-[327px] aspect-video">
-            <img
-              src="/Destination1.png"
-              alt=""
-              className="object-cover object-center transition-opacity ease-in-out duration-300 opacity-100"
-            />
-          </div>
-        </div>
-        <div className="py-4 lg:py-[72px] w-full flex flex-col lg:odd:flex-row-reverse lg:flex-row gap-4 lg:gap-6 items-stretch">
-          <div className="lg:w-1/2 flex flex-col justify-between space-y-2 lg:space-y-4 text-center lg:text-left">
-            <div className="w-full flex flex-col space-y-2">
-                <span className="text-left text-xs lg:text-base">7 DAYS 6 NIGHTS</span>
-                <strong className="text-left text-dark-aquaman text-base lg:text-4xl font-bold line-clamp-2">Paradise Gateway: Labuan Bajo</strong>
-                <span className="text-left text-dark-teal text-sm lg:text-base line-clamp-1 font-bold">Organized by Pandooin</span>
-                <p className="text-left text-sm lg:text-base text-dark-teal line-clamp-4">Labuan Bajo is a tropical paradise nestled in the westernmost part of Flores Island, Indonesia. With its stunning landscapes, crystal-clear waters, and vibrant marine life, it's a gateway to explore the mesmerizing Komodo National Park.</p>
-            </div>
-            <div className="mt-auto w-full inline-flex justify-between items-center">
-                <div className="flex flex-col">
-                    <span className="text-left text-dark-teal text-xs lg:text-base">Start from</span>
-                    <span className="text-gray-400 line-through font-medium text-sm lg:text-3xl">IDR 9,999,999</span>
-                    <span className="text-left font-unbounded text-dark-teal text-lg lg:text-[28px] font-medium">IDR 5,200,000</span>
-                </div>
-                <a href="#" className="mt-10">
-                    <button className="button text-center inline-flex justify-center items-center px-6 py-2.5 rounded-full capitalize font-bold text-sm lg:text-base transition-colors ease-in-out duration-300 bg-transparent text-dark-teal border-2 border-dark-teal hover:bg-dark-teal hover:text-vista-white hover:border-dark-teal ">
-                    See Details
-                    </button>
-                </a>
-            </div>
-          </div>
-          <div className="relative lg:w-1/2 max-h-[256px] lg:max-h-[327px] aspect-video">
-            <img
-              src="/Destination1.png"
-              alt=""
-              className="object-cover object-center transition-opacity ease-in-out duration-300 opacity-100"
-            />
-          </div>
-        </div>
+      {itineraries.map((itinerary, index) => (
+        <>
+        <div key={index} className="py-4 lg:py-[72px] w-full flex flex-col lg:odd:flex-row-reverse lg:flex-row gap-4 lg:gap-6 items-stretch">
+         <div className="lg:w-1/2 flex flex-col justify-between space-y-2 lg:space-y-4 text-center lg:text-left">
+           <div className="w-full flex flex-col space-y-2">
+               <span className="text-left text-xs lg:text-base">{itinerary.itinerary_day} {itinerary.itinerary_day > 1 ? 'DAYS' : 'DAY'}</span>
+               <strong className="text-left text-dark-aquaman text-base lg:text-4xl font-bold line-clamp-2">{itinerary.itinerary_name}</strong>
+               <span className="text-left text-dark-teal text-sm lg:text-base line-clamp-1 font-bold">Organized by {itinerary.partner_name}</span>
+               <p className="text-left text-sm lg:text-base text-dark-teal line-clamp-4">{itinerary.itinerary_short_description}</p>
+           </div>
+           <div className="mt-auto w-full inline-flex justify-between items-center">
+               <div className="flex flex-col">
+                   <span className="text-left text-dark-teal text-xs lg:text-base">Start from</span>
+                   {itinerary.related_variant?.itinerary_variant_disc_price != 0 ? (
+                     <span className="text-gray-400 line-through font-medium text-sm lg:text-3xl">IDR {new Intl.NumberFormat('id-ID').format(itinerary.related_variant?.itinerary_variant_disc_price)}</span>
+                  
+                   ):(<></>)}
+
+                   
+                   <span className="text-left font-unbounded text-dark-teal text-lg lg:text-[28px] font-medium">IDR {new Intl.NumberFormat('id-ID').format(itinerary.related_variant?.itinerary_variant_pub_price)}</span>
+               </div>
+               <a href="#" className="mt-10">
+                   <button className="button text-center inline-flex justify-center items-center px-6 py-2.5 rounded-full capitalize font-bold text-sm lg:text-base transition-colors ease-in-out duration-300 bg-transparent text-dark-teal border-2 border-dark-teal hover:bg-dark-teal hover:text-vista-white hover:border-dark-teal ">
+                   See Details
+                   </button>
+               </a>
+           </div>
+         </div>
+      <div className="relative lg:w-1/2 max-h-[256px] lg:max-h-[327px] aspect-video">
+      <Swiper
+      spaceBetween={30}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 2500, disableOnInteraction: false }}
+      className="mySwiper"
+    >
+          {itinerary.related_galleries.map((gallery, index) => (
+            <> 
+             <SwiperSlide key={index}>
+              <img
+                src={`https://ik.imagekit.io/pandooin/tr:pr-true/${gallery.gallery_path}`}
+                alt={gallery.gallery_alt_text}
+                className="object-cover object-center transition-opacity ease-in-out duration-300 opacity-100"
+              />
+             </SwiperSlide>
+            
+            </>
+             
+          ))}
+           </Swiper>
+      </div>
+       </div>
+        </>
+      ))}
+        
       </div>
     </section>
     <section className="w-full max-w-7xl mx-auto py-[54px] lg:py-[26px] px-4">
